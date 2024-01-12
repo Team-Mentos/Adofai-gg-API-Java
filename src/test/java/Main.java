7c6ab60fcbf7718763879f7886a46391e773a330
Jongyeol <45934107+Jongye0l@users.noreply.github.com>
2024-01-13 00:24:04 +0900

    fix: group, version

--- src/test/java/Main.java
+++
@@ -1,8 +0,0 @@
-import xyz.krmentos.adofaigg.AdofaiGG;
-
-public class Main {
-    public static void main(String[] args) {
-        AdofaiGG adofaiGG = new AdofaiGG();
-        System.out.println(adofaiGG.getMapById(1).getSong());
-    }
-}
